89e5c23b68a458f18a9cc1f23698c4152a165fdd
thangvn0987 <thangnm7509@ut.edu.vn>
2025-10-13 20:23:04 +0700

    setupdocker2 and gateway

--- services/ai-service/server.js
+++ services/ai-service/server.js
@@ -1,2 +1,2 @@
-console.log('ai-service placeholder running on PORT', process.env.PORT || 3000);
+console.log("ai-service placeholder running on PORT", process.env.PORT || 3000);
 setInterval(() => {}, 1 << 30);
--- services/learner-service/server.js
+++ services/learner-service/server.js
@@ -1,2 +1,5 @@
-console.log('learner-service placeholder running on PORT', process.env.PORT || 3000);
+console.log(
+  "learner-service placeholder running on PORT",
+  process.env.PORT || 3000
+);
 setInterval(() => {}, 1 << 30);
